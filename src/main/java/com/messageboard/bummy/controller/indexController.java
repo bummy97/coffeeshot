2115494c543b9fb07ea994749fcd59d6d9e0c1c7
bummy <dydqja4582@naver.com>
2022-10-02 00:51:05 +0900

    화면(index, 메뉴만들기, 메뉴 조회), 컨트롤러 만들기

---
+++ src/main/java/com/messageboard/bummy/controller/indexController.java
@@ -0,0 +1,22 @@
+package com.messageboard.bummy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("makelist")
+    public String makelist(){
+        return "makelist";
+    }
+
+    @GetMapping("menulist")
+    public String menulist(){
+        return "menulist";
+    }
+}
